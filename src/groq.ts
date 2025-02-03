f3b3d0fc6a44497000aa60ea3f0bc219c29cad17
Maciek Sitkowski <msitkowski94@gmail.com>
2025-02-03 09:07:49 +0100

    add stronghold plugin

--- src/groq.ts
+++ src/groq.ts
@@ -1,17 +1,16 @@
 import Groq, {toFile} from 'groq-sdk'
-import {env, modelNames} from './constants'
 
 export const groq = {
 	transcribe,
 }
 
-const client = new Groq({apiKey: env.GROQ_API_KEY, dangerouslyAllowBrowser: true})
+async function transcribe(buffer: ArrayBuffer, apiKey: string) {
+	const client = new Groq({apiKey, dangerouslyAllowBrowser: true})
 
-async function transcribe(buffer: ArrayBuffer) {
 	const transcription = await client.audio.transcriptions.create({
 		file: await toFile(buffer, 'audio.wav'),
 		prompt: `User is providing his life metrics, like weight, sleep, steps, etc.`,
-		model: modelNames.whisperLargeV3,
+		model: 'whisper-large-v3',
 	})
 
 	return transcription.text
