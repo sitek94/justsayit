eb21b70d1b85c7232569b1b663c959e2c663f632
Maciek Sitkowski <msitkowski94@gmail.com>
2025-02-16 16:29:39 +0100

    bump deps

--- src/lib/settings.svelte
+++ src/lib/settings.svelte
@@ -49,7 +49,7 @@
 
 	<button
 		type="submit"
-		class="rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+		class="rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none"
 	>
 		Save Settings
 	</button>
